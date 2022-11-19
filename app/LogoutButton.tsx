d770bef0393c7bed566e6660ea7cb433864502fa
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2022-11-19 12:42:08 -0300

    feat: logout button

--- app/LogoutButton.tsx
+++ app/LogoutButton.tsx
@@ -1,13 +1,15 @@
-"use client";
+'use client'
+
+import { signOut } from 'next-auth/react'
 
 function LogoutButton() {
   return (
-    <button className="px-4 py-2 font-bold text-white bg-blue-400 rounded hover:bg-blue-500"
-      onClick={() => {}}
-    >
+    <button
+      className="px-4 py-2 font-bold text-white bg-blue-400 rounded hover:bg-blue-500"
+      onClick={() => signOut()}>
       Sign Out
     </button>
-  );
+  )
 }
 
-export default LogoutButton;
+export default LogoutButton
